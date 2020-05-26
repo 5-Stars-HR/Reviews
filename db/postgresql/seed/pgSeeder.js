35165c77b9494dce97e0204807c80fc9630d8e32
Toan Tran <trantoankk@gmail.com>
2020-05-25 19:10:46 -0700

    fixed white spaces

--- db/postgresql/seed/pgSeeder.js
+++ db/postgresql/seed/pgSeeder.js
@@ -2,7 +2,6 @@ const faker = require('faker');
 const fs = require('fs');
 const path = require('path');
 
-
 const seedUsers = () => {
   let fileContent = ''
   const randomAge = [5, 10, 30, 40, 13, 15, 34, 32, 21 , 5, 9, 12, 22, 14, 17, 19, 30, 20, 22, 23];
@@ -40,6 +39,7 @@ const seedProducts = (quantity) =>  {
 
 const seedReviews = (productQuantity) => {
   const filePath = path.join(__dirname, '../data/pgReviews.txt');
+  let writeStream = fs.createWriteStream(filePath, { flags : 'w+'});
   let fileContent = '';
   let fileIndex = 1;
   let index = 0;
@@ -51,8 +51,6 @@ const seedReviews = (productQuantity) => {
   const randomDiff = [5, 4, 1, 5, 2, 3, 5, 4, 5, 4, 5, 4, 1, 2, 3, 5, 4, 1, 2, 3];
   const randomValue = [5, 5, 5, 4, 4, 4, 1, 5, 4, 1, 4, 5, 3, 2, 5, 4, 1, 2, 3, 5];
   const randomBuildTimes = [10, 20, 15, 8, 9, 40, 70, 100, 30, 40, 60, 80, 13, 14, 63, 54, 67, 23, 4, 5];
-
-  let writeStream = fs.createWriteStream(filePath, { flags : 'w+'});
   for (let i = 1; i <= productQuantity; i += 1) {
     for (let j = 1; j <= randomReviews[reviewIndex]; j += 1) {
       const createdAt = faker.date.between('2020-01-01', '2020-05-05').toString().replace(/G.+/g, 'PST');
@@ -68,13 +66,12 @@ const seedReviews = (productQuantity) => {
       const buildTime = randomBuildTimes[index];
 
       fileContent += `${userId}, ${i}, '${createdAt}', '${subject}', '${comment}', ${rating}, ${recommended}, ${isHelpful}, ${isNotHelpful}, ${playExperience}, ${difficulty}, ${valueForMoney},${buildTime}\n`;
-
       //increment or reset index
       (index === 19) ? index = 0 : index += 1;
       (userId === 100) ? userId = 1 : userId += 1;
     }
     (reviewIndex === 19) ? reviewIndex = 0 : reviewIndex += 1;
-
+    //reset filecontent ecery 300k entries due to string max-length of V8
     if ((i+1) % 300000 === 0) {
       writeStream.write(fileContent, (err) => {
         if (err) {
@@ -87,7 +84,7 @@ const seedReviews = (productQuantity) => {
       fileContent = '';
     }
   }
-  // let writeStream = fs.createWriteStream(filePath, { flags : 'w+'});
+  //write the remaining entries
   writeStream.write(fileContent, (err) => {
     if (err) {
       console.log('error while writing pgData' , err)
