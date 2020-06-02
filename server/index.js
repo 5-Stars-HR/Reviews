8075249bca32f197fe6d8412be34d936ec4ca80d
Toan Tran <trantoankk@gmail.com>
2020-06-02 11:09:05 -0700

    changed port to 3000

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 require('newrelic');
 const app = require('./server');
-const port = 8080;
+const port = 3000;
 
 app.listen(port, () => console.log(`Server is listening at http://localhost:${port}`));
